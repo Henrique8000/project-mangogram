3fa2602d000209069301f7440b67ac7d65dbd606
Henrique <henriqueflavioguimaraes@gmail.com>
2026-09-09 00:27:06 -0300

    Project created!

---
+++ src/main/java/com/henriquedev/mangogram/MangogramApplication.java
@@ -0,0 +1,13 @@
+package com.henriquedev.mangogram;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MangogramApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MangogramApplication.class, args);
+	}
+
+}
